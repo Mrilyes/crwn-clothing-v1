783a36b82fe5d3dee49c0ed7166b416a66af3f3b
Mrilyes <ilyesghlila1@gmail.com>
2023-07-26 23:24:27 +0100

    p9

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ import Home from "./routes/home/home.component";
 
 import Authentication from "./routes/authentication/authentication.component";
 
+import Shop from "./routes/shop/shop.component";
 
 
-const Shop = () => {
-  return <h1> I am the shop page </h1>;
-};
 
 const App = () => {
   return (
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,10 +4,17 @@ import { Fragment , useContext} from "react";
 // link same as encre tag
 import { Outlet , Link } from "react-router-dom";
 
-import { ReactComponent as CrwnLogo } from "../../assets/083 crown.svg";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { UserContext } from "../../contexts/user.context";
 
+import { CartContext } from "../../contexts/cart.context";
+ 
 import './navigation.styles.scss'
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -16,9 +23,9 @@ const Navigation = () => {
 
   const { currentUser } = useContext(UserContext);
   // console.log(currentUser);
+  const { isCartOpen }  = useContext(CartContext);
 
-
-  return (
+  return ( 
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
@@ -37,7 +44,9 @@ const Navigation = () => {
               </Link>
             )
           }
+          <CartIcon/>
         </div>
+        {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet />
     </Fragment>
